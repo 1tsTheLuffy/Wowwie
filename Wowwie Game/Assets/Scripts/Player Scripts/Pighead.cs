376654503c1af40596dd0e80922ecf34b9d954b6
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-01-18 23:56:55 +0530

    10th Commit..

--- Wowwie Game/Assets/Scripts/Player Scripts/Pighead.cs
+++ Wowwie Game/Assets/Scripts/Player Scripts/Pighead.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Pighead : MonoBehaviour
 {
-    private float x;
-    [SerializeField] float speed;
+    private bool isGrounded;
+    [SerializeField] float jumpForce;
+
+    Transform Player;
+
+    [SerializeField] LayerMask Ground;
 
     PlayerController pc;
 
@@ -14,12 +18,17 @@ public class Pighead : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
         pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     private void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(transform.position, .5f, Ground);
+
+        if(isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce * Time.deltaTime;
+        }
     }
 }
